7635ea3d6696857476bb217f33a1f7ab2b0d0d95
oskyrep <abdulrahman_098@hotmail.com>
2021-01-07 16:03:26 +0500

    freertos code not working

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,33 @@
-/*********
-  Rui Santos
-  Complete project details at https://randomnerdtutorials.com  
-*********/
+#define DATA_ACQUISITION_FREQ 1
 #include <Arduino.h>
-#include <esp32-mqtt.h>
-#include <Storage.h>
+#include <FreeRTOS.h>
 #include <rtc.h>
+#include <Storage.h>
 #include <can.h>
 #include <bluetooth.h>
-#include <FreeRTOS.h>
-TaskHandle_t Task1;
-TaskHandle_t Task2;
-void Task1code( void * pvParameters );
-void Task2code( void * pvParameters );
-// LED pins
-const int led1 = 2;
-const int led2 = 4;
+#include "esp32-mqtt.h"
+
+String towrite, toread;
+
+void vAcquireData( void *pvParameters );
+void vBlTransfer( void *pvParameters );
+void vStorage( void *pvParameters );
+void vWifiTransfer( void *pvParameters );
+
+SemaphoreHandle_t semaAqData1, seamAqData2, semaBlTx1, semaStorage1, seamStorage2, semaWifi1;
+void addSlotsData(String B_Slot,String B_ID,String B_Auth, String B_Age,String B_Type ,String B_M_Cycles ,String B_U_Cycles , 
+                    String B_Temp, String B_SoC, String B_SoH, String B_RoC,String B_Vol ,String B_Curr){
+    towrite += B_Slot + "," + B_ID + "," + B_Auth + "," + B_Age + "," + B_Type + "," + B_M_Cycles + "," + 
+            B_U_Cycles + "," + B_Temp + "," + B_SoC + "," + B_SoH + "," + B_RoC + "," + B_Vol + "," + B_Curr;
+    return;
+}
+
+
 
 void setup() {
-  Serial.begin(115200); //Start Serial monitor
+    Serial.begin(115200); //Start Serial monitor
     pinMode(LED_BUILTIN, OUTPUT);
-    pinMode(2, OUTPUT);
+    if(WiFi.status() == WL_CONNECTED){digitalWrite(2, 1);}
     //setupCloudIoT();
     //bt.init();
     initRTC();
@@ -33,60 +40,137 @@ void setup() {
             delay(1000);
         }
     }
-  pinMode(led1, OUTPUT);
-  pinMode(led2, OUTPUT);
-
-  //create a task that will be executed in the Task1code() function, with priority 1 and executed on core 0
-  xTaskCreatePinnedToCore(
-                    Task1code,   /* Task function. */
-                    "Task1",     /* name of task. */
-                    10000,       /* Stack size of task */
-                    NULL,        /* parameter of the task */
-                    1,           /* priority of the task */
-                    &Task1,      /* Task handle to keep track of created task */
-                    0);          /* pin task to core 0 */                  
-  delay(500); 
-
-  //create a task that will be executed in the Task2code() function, with priority 1 and executed on core 1
-  xTaskCreatePinnedToCore(
-                    Task2code,   /* Task function. */
-                    "Task2",     /* name of task. */
-                    10000,       /* Stack size of task */
-                    NULL,        /* parameter of the task */
-                    1,           /* priority of the task */
-                    &Task2,      /* Task handle to keep track of created task */
-                    1);          /* pin task to core 1 */
-    delay(500); 
+    /*semaAqData1 = xSemaphoreCreateBinary();
+    seamAqData2 = xSemaphoreCreateBinary();
+    semaBlTx1 = xSemaphoreCreateBinary();
+    semaStorage1 = xSemaphoreCreateBinary();
+    seamStorage2 = xSemaphoreCreateBinary();
+    semaWifi1 = xSemaphoreCreateBinary();
+    xSemaphoreGive(semaAqData1);
+    xSemaphoreGive(seamAqData2);*/
+    Serial.println("creating all tasks");
+    Serial.flush();
+    TaskHandle_t dataTask, blTask, storageTask, wifiTask;
+    xTaskCreatePinnedToCore(vAcquireData, "Data Acquisition", 200, NULL, 2, &dataTask, 0);
+    //xTaskCreatePinnedToCore(vBlTransfer, "Bluetooth Transfer", 200, NULL, 1, &blTask, 0);
+    //xTaskCreatePinnedToCore(vStorage, "Storage Handler", 200, NULL, 2, &storageTask, 1);
+    //xTaskCreatePinnedToCore(vWifiTransfer, "Transfer data on Wifi", 200, NULL, 1, &wifiTask, 1);
+    Serial.println("created all tasks");
+    //vTaskStartScheduler();
 }
-
-//Task1code: blinks an LED every 1000 ms
-void Task1code( void * pvParameters ){
-  Serial.print("Task1 running on core ");
-  Serial.println(xPortGetCoreID());
-
-  for(;;){
-    digitalWrite(led1, HIGH);
-    delay(1000);
-    digitalWrite(led1, LOW);
-    delay(1000);
-  } 
-}
-
-//Task2code: blinks an LED every 700 ms
-void Task2code( void * pvParameters ){
-  Serial.print("Task2 running on core ");
-  Serial.println(xPortGetCoreID());
-
-  for(;;){
-      Serial.print("Task2 running on core ");
-  Serial.println(xPortGetCoreID());
-    digitalWrite(led2, HIGH);
-    delay(700);
-    digitalWrite(led2, LOW);
-    delay(700);
-  }
-}
-
+unsigned long lastMillis = 0;
+String CloudData = "";
 void loop() {
-  
+    
+    /*if(WiFi.status() == WL_CONNECTED){digitalWrite(2, 1);}
+    
+    
+    
+    
+    mqtt->loop();
+    delay(10);  // <- fixes some issues with WiFi stability
+    if (!mqttClient->connected()) {
+        connect();
+    }
+    if (mqttClient->connected()) {
+        Serial.println("*****");
+        Serial.println(publishTelemetry(towrite));
+        Serial.println(ESP.getFreeHeap());
+        //Serial.println(publishTelemetry("hi"));
+        Serial.println("*****");
+    }
+    storage.write_data(getTime2(), towrite);
+    towrite= "";
+    towrite = storage.read_data();
+    if(towrite != ""){
+        storage.write_data("copy",towrite);
+        storage.mark_data(getTime2());
+    }
+    Serial.println(towrite);
+    static long counter = 0;
+    counter++;
+    Serial.println(counter);
+    Serial.println();
+    delay(1000);*/
+}
+void vAcquireData( void *pvParameters ){
+    TickType_t xLastWakeTime;
+    const TickType_t xFrequency = DATA_ACQUISITION_FREQ;
+
+    for(;;){
+        //TODO: handle the problem when semaphore is not available within the defined time. and also define the blocking time
+        //xSemaphoreTake(semaAqData1, portMAX_DELAY);
+        //xSemaphoreTake(seamAqData2, portMAX_DELAY);
+        //Dummy acquisition of data
+        //we need to place a valid CSV string in towrite string
+        towrite = "";
+        towrite += getTime() + ",";                 //time
+        towrite += String("BSS1715001") + ",";      //BSSID
+        towrite += String("16") + ",";              //total slots
+        towrite += String("20.273") + ",";              //BSS voltage
+        towrite += String("55.781") + ",";              //BSS CURRENT
+        towrite += String("7400") + ",";                  //BSS POWER
+        towrite += String("0.8") + ",";                 //BSS power factor
+        //addSlotsData(String B_Slot,String B_ID,String B_Auth, String B_Age,String B_Type ,String B_M_Cycles ,
+        //             String B_U_Cycles , String B_Temp, String B_SoC, String B_SoH, String B_RoD,String B_Vol ,String B_Curr)
+        addSlotsData("01", "1718953129", "BSS22", "22", "2211", "500", "200", "30", "80", "50", "22", "12.371", "20.561");towrite += ",";
+        addSlotsData("02", "1718953130", "BSS22", "22", "2211", "500", "200", "30", "80", "50", "22", "12.371", "20.561");towrite += ",";
+        addSlotsData("03", "1718953131", "BSS22", "22", "2211", "500", "200", "30", "80", "50", "22", "12.371", "20.561");towrite += ",";
+        addSlotsData("04", "1718953128", "BSS22", "22", "2211", "500", "200", "30", "80", "50", "22", "12.371", "26.561");towrite += ",";
+        addSlotsData("05", "1718953127", "BSS22", "22", "2211", "500", "200", "30", "80", "50", "22", "12.371", "20.561");towrite += ",";
+        addSlotsData("06", "1718953126", "BSS22", "22", "2211", "500", "200", "30", "80", "50", "22", "12.371", "20.561");towrite += ",";
+        addSlotsData("07", "1718953125", "BSS22", "22", "2211", "500", "200", "30", "80", "50", "22", "12.371", "20.561");towrite += ",";
+        addSlotsData("08", "1718953124", "BSS22", "22", "2211", "500", "200", "30", "80", "50", "22", "12.371", "26.561");towrite += ",";
+        addSlotsData("09", "1718953123", "BSS22", "22", "2211", "500", "200", "30", "80", "50", "22", "12.371", "20.561");towrite += ",";
+        addSlotsData("10", "1718953122", "BSS22", "22", "2211", "500", "200", "30", "80", "50", "22", "12.371", "20.561");towrite += ",";
+        addSlotsData("11", "1718953121", "BSS22", "22", "2211", "500", "200", "30", "80", "50", "22", "12.371", "20.561");towrite += ",";
+        addSlotsData("12", "1718953120", "BSS22", "22", "2211", "500", "200", "30", "80", "50", "22", "12.371", "26.561");towrite += ",";
+        addSlotsData("13", "1718953119", "BSS22", "22", "2211", "500", "200", "30", "80", "50", "22", "12.371", "20.561");towrite += ",";
+        addSlotsData("14", "2718953129", "BSS22", "22", "2211", "500", "200", "30", "80", "50", "22", "12.371", "20.561");towrite += ",";
+        addSlotsData("15", "1518953129", "BSS22", "22", "2211", "500", "200", "30", "80", "50", "22", "12.371", "20.561");towrite += ",";
+        addSlotsData("16", "1718253129", "BSS22", "22", "2211", "500", "200", "30", "80", "50", "22", "12.371", "26.561");
+        Serial.println("Acquire data task ended!");
+        //xSemaphoreGive(semaBlTx1);
+        //xSemaphoreGive(semaStorage1);
+        //Now towrite string contains one valid string of CSV data chunk
+        vTaskDelayUntil(&xLastWakeTime, xFrequency);
+    }
+}
+
+void vBlTransfer( void *pvParameters ){
+    for(;;){
+        xSemaphoreTake(semaBlTx1,portMAX_DELAY);
+        //transfer data to bluetooth
+        Serial.println("data to bluetooth transferred");
+        xSemaphoreGive(semaAqData1);
+    }
+}
+
+void vStorage( void *pvParameters ){
+    for(;;){
+        xSemaphoreTake(semaStorage1,portMAX_DELAY);
+        xSemaphoreTake(semaWifi1,portMAX_DELAY);
+        //storage.write_data(getTime2(), towrite);
+        digitalWrite(2, !digitalRead(2));
+        xSemaphoreGive(seamAqData2);
+    }
+}
+
+void vWifiTransfer( void *pvParameters ){
+    TickType_t xLastWakeTime;
+    const TickType_t xFrequency = 2;
+    for(;;){
+        xSemaphoreTake(semaWifi1,portMAX_DELAY);
+        //if(WiFi.status() == WL_CONNECTED){digitalWrite(2, 1);}
+        //else    digitalWrite(2,0);
+        if(1){
+            Serial.println("data sent over wifi");
+        } else{
+            vTaskDelayUntil(&xLastWakeTime, xFrequency);
+        }
+        xSemaphoreGive(semaWifi1);
+        //check unsent data and send data over wifi
+        //also take semaWifi1 when starting to send one chunk of data and give semaWifi1 when sending of one chunk of data is complete
+    }
+    
 }
